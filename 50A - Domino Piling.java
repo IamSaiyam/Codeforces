f0423aad5f28d82f74e5fd4bc9fc8d4011254fbf
Saiyam Jain <59947799+saiyamhk@users.noreply.github.com>
2022-01-25 20:53:29 +0530

    Create 50A - Domino Piling.java

---
+++ 50A - Domino Piling.java
@@ -0,0 +1,11 @@
+//143989646	Jan/25/2022 20:52UTC+5.5	sym	50A - Domino piling	Java 8	Accepted	248 ms	0 KB
+import java.util.Scanner;
+
+public class DominoPiling {
+    public static void main(String... args){
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        System.out.println((m*n)/2);
+    }
+}
